4e381a2d416ad72be6f945c6f0db981828b4be82
JC <nucasa.jerick@gmail.com>
2024-08-23 22:24:22 +0800

    ban4 more backend changes

--- server/models/user_model.js
+++ server/models/user_model.js
@@ -1,57 +1,42 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
+  is_archived: Boolean,
+  password: String,
+  u_fname: String,
+  u_mname: String,
+  u_lname: String,
+  u_dep: String,
+  u_datehired: String,
+
   usertype: {
     type: String,
     enum: ["student", "admin", "teacher", "asp", "ntp"], //different types
-    required: true,
   },
-  u_fname: {
-    type: String,
-    required: true,
-  },
-  u_mname: {
-    type: String,
-    required: true,
-  },
-  u_lname: {
-    type: String,
-    required: true,
-  },
+
   u_mnum: {
     type: String,
-    required: true,
     unique: true,
   },
-  u_dep: {
+
+  u_studnum: {
+    //for student
     type: String,
-    required: true,
-  },
-  u_studnum: { //for student
-    type: String,
-    required: true,
     unique: true,
   },
-  u_datehired: { //for non-student
-    type: String,
-    required: true,
-  },
+
   email: {
     type: String,
-    required: true,
     unique: true,
     validate: {
-      validator: function(v) {
-        return v.endsWith('nu-moa.edu.ph');
+      validator: function (v) {
+        return v.endsWith("nu-moa.edu.ph");
       },
-      message: props => `${props.value} is not a valid email. It should end with '@nu-moa.edu.ph'`
+      message: (props) =>
+        `${props.value} is not a valid email. It should end with '@nu-moa.edu.ph'`,
     },
-  },
-  password: {
-    type: String,
-    required: true,
   },
 });
 
@@ -67,14 +52,14 @@ UserSchema.pre("save", function (next) {
   next();
 });
 
-UserSchema.methods.generateAuthToken = function() {
+UserSchema.methods.generateAuthToken = function () {
   const token = jwt.sign(
     { _id: this._id, usertype: this.usertype },
-    'COMEX2024',
-    { expiresIn: '1h' }  // Token expires in 1 hour
+    "COMEX2024",
+    { expiresIn: "1h" } // Token expires in 1 hour
   );
   return token;
-}
+};
 
 const User = mongoose.model("User", UserSchema);
 module.exports = User;
