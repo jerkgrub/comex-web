eaf842c67676d992aea3e3be2527e582e198b038
jerkgrub <nucasa.jerick@gmail.com>
2024-10-14 16:54:34 +0800

    linux test

--- src/pages/preAuth/HeroPage.jsx
+++ src/pages/preAuth/HeroPage.jsx
@@ -36,7 +36,7 @@ const HeroPage = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1, ease: "easeOut" }}
         >
-          Unlock Your Community Impact!
+          Error
         </motion.p>
 
         {/* Subheading */}
